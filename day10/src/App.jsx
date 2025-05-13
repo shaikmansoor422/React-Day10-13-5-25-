9d40d9dcbc88af8f2dcfabae57d67a03ac339b2c
Mansoor shaik <125723822+shaikmansoor422@users.noreply.github.com>
2025-05-13 16:37:26 +0530

    first commit of class based components

---
+++ day10/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Myapp extends React.Component{
+  render(){
+    return(
+      <>
+      <div>Hello this is my first class based component</div>
+      </>
+    )
+  }
+  
+}
+
+export default Myapp
